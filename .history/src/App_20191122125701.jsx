22a16bee2bf80f12c08fe8a170469b007881ce96
Harshit Sahu <hsahu@partner.aligntech.com>
2019-11-22 13:56:02 +0530

    user Profile

---
+++ .history/src/App_20191122125701.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/destructuring-assignment */
+import React from "react";
+import Header from "./Components/Header";
+import ShotMapSectionHeader from "./Components/ShotMapSectionHeader";
+import ShotMapGridContainer from "./Components/ShotMapGridContainer";
+import UserRoles from "./Components/UserRoles";
+
+const checkBoxOption = [
+  {
+    checked: true,
+    checkBoxLabel: "Controls"
+  },
+  {
+    checked: true,
+    checkBoxLabel: "Reticle"
+  },
+  {
+    checked: true,
+    checkBoxLabel: "ShotMap"
+  }
+];
+
+function App() {
+  const [option, setOption] = React.useState(checkBoxOption);
+  function handleFilter(updatedOption) {
+    console.log(updatedOption);
+    setOption(updatedOption);
+  }
+  return (
+    <div>
+      <Header />
+      <UserRoles />
+      <ShotMapSectionHeader
+        checkBoxOption={option}
+        optionSelected={handleFilter}
+      />
+      <ShotMapGridContainer gridEnable={option} />
+    </div>
+  );
+}
+
+export default App;
+
+class Apap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { bsjdbfvs: 0 };
+    alert("dfsvsf");
+  }
+  // componentDidMount() {
+
+  // }
+
+  render() {
+    return <div>{this.state.bsjdbfvs}</div>;
+  }
+}
